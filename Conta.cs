7955527efd23a0671acc23c22f39625121b17b49
Marcio Miranda <Marcio_lgm@outlook.com>
2023-09-30 15:45:12 -0100

    Iniciando com classes

---
+++ Conta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX00
+{
+    public class Conta
+    {
+        public double Saldo { get; set; }
+        public double Limite { get; set; }
+        public int Numero { get; set; }
+
+
+    }
+}
